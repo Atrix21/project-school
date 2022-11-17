9cc0ca77502c4a4c399b694b9be91097803a0b67
Aditya <93080929+Atrix21@users.noreply.github.com>
2022-11-17 12:37:25 +0530

    task1 completed

---
+++ Week_2/task_1/task_1.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var linebreak = document.createElement("br");
+    var linebreak_1 = document.createElement("br");
+    document.getElementById("txt").append(["NGIT"]);
+    document.getElementById("txt").append(linebreak);
+    document.getElementById("txt").append(["NGIT"]);
+    document.getElementById("txt").append(linebreak_1);
+}
