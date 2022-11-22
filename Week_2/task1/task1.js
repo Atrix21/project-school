cb24d5532888166131971653786128d921dee5e3
Aditya <93080929+Atrix21@users.noreply.github.com>
2022-11-22 11:12:51 +0530

    need to add carousels

--- Week_2/task1/task1.js
+++
@@ -1,8 +0,0 @@
-function myFunction(){
-    var linebreak = document.createElement("br");
-    var linebreak1 = document.createElement("br");
-    document.getElementById("txt").append(["NGIT"]);
-    document.getElementById("txt").append(linebreak);
-    document.getElementById("txt").append(["NGIT"]);
-    document.getElementById("txt").append(linebreak1);
-}
