020f868b247d3549693decb8cdbca717ca4c8001
Aditya <93080929+Atrix21@users.noreply.github.com>
2022-12-01 00:06:21 +0530

    week 3 tasks uploaded

---
+++ Week_3/task1/task_1.js
@@ -0,0 +1,5 @@
+function myFunc(){
+    document.getElementById("p1").style.color="red";
+    document.getElementById("p3").style.color="red";
+    document.getElementById("p5").style.color="red";
+}
